80d818452fb02e0194552da482cfae4c4273ea2b
Malleusd <Malleusdraconia63@gmail.com>
2022-11-11 11:56:51 +0700

    fon

---
+++ assignment.js
@@ -0,0 +1,6 @@
+let x = 20;
+
+//1.Assignment and Addition
+console.log('x += 10');
+
+//2.Assi
---
+++ comparison.js
@@ -0,0 +1,15 @@
+//1.epual operator
+console.log(2 == 2) //true
+console.log(2 == '2')//true
+
+//2.not epual operator
+console.log(3 != 2)//true
+console.log('hello' != "Hello")//true
+
+//3.stri epual operator
+console.log(2 === 2)//true
+console.log(2 === '2')//false
+
+//4.stri not epual operator
+console.log(2 !== 2)//true
+console.log(2 !== '2')//false
